38d4111eb768a87718c4e5aa53780f9d566ba2b5
Jason Ernst <ernstjason1@gmail.com>
2021-09-08 21:16:32 -0700

    Added github actions workflow, fixed unit tests

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import de.mannodermaus.junit5.ActivityScenarioExtension;
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -18,5 +20,7 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.myapplication", appContext.getPackageName());
+
+        ActivityScenarioExtension.launch(MainActivity.class);
     }
 }
