8650531a2953cdaf765bf41606b97bbb51d2efe3
Jason Ernst <ernstjason1@gmail.com>
2021-09-08 20:33:13 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # hello-java-android
+
+When starting a new project, I always find getting the tools up and running quickly is a pain, so I
+wanted something for java / android which already had everything setup and working.
+
+This is a WiP inspired by a similar repo for Kotlin / Android: https://github.com/compscidr/hello-kotlin-android
+
+This aims to be a starter repo with all of the following tooling setup:
+* [Junit5](https://junit.org/junit5/docs/current/user-guide/) (unit + integration tests)
+
+TODO:
+* [Jacoco](https://www.eclemma.org/jacoco/)
+* [Github actions](https://github.com/marketplace/actions/gradle-android)
+* [Codecov](https://codecov.io/)
---
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import android.content.Context;
+import org.junit.jupiter.api.Test;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.myapplication", appContext.getPackageName());
+    }
+}
