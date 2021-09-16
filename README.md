ec29e0fabbef542bbf61358404f43a8990672dca
Jason Ernst <ernstjason1@gmail.com>
2021-09-15 18:54:02 -0700

    fixed coverage

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # hello-java-android
 [![Build Status](https://github.com/compscidr/hello-java-android/workflows/Gradle%20Build/badge.svg)](https://github.com/compscidr/hello-java-android/actions)&nbsp;
 [![codecov](https://codecov.io/gh/compscidr/hello-java-android/branch/master/graph/badge.svg)](https://codecov.io/gh/compscidr/hello-java-android)&nbsp;
+
 When starting a new project, I always find getting the tools up and running quickly is a pain, so I
 wanted something for java / android which already had everything setup and working.
 
 This is a WiP inspired by a similar repo for Kotlin / Android: https://github.com/compscidr/hello-kotlin-android
 
 This aims to be a starter repo with all of the following tooling setup:
+* [Github actions](https://github.com/marketplace/actions/gradle-android)
 * [Junit5](https://junit.org/junit5/docs/current/user-guide/) (unit + integration tests)
+* [Jacoco](https://www.eclemma.org/jacoco/)
+* [Codecov](https://codecov.io/)
 
 TODO:
-* [Jacoco](https://www.eclemma.org/jacoco/)
-* [Github actions](https://github.com/marketplace/actions/gradle-android)
-* [Codecov](https://codecov.io/)
--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import android.content.Context;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import de.mannodermaus.junit5.ActivityScenarioExtension;
@@ -15,12 +17,18 @@ import de.mannodermaus.junit5.ActivityScenarioExtension;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleInstrumentedTest {
+    @RegisterExtension
+    ActivityScenarioExtension<MainActivity> scenarioExtension = ActivityScenarioExtension.launch(MainActivity.class);
+
     @Test
-    public void useAppContext() {
+    public void useAppContext(ActivityScenario<MainActivity> scenario) {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.myapplication", appContext.getPackageName());
 
-        ActivityScenarioExtension.launch(MainActivity.class);
+        scenario.onActivity(activity -> {
+
+        });
+
     }
 }
