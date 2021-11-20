fb5dad393761e37ff29ce4bc9c6a44b9625997e6
Jason Ernst <ernstjason1@gmail.com>
2021-11-20 11:16:27 -0800

    added a test that covers something that the connectedTest doesn't to show that both are working

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -13,5 +14,15 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    /*
+     * Test something that the connectedTest doesn't so that we can see that both the off and on
+     * device coverage is working.
+     */
+    @Test
+    public void testSomethingForCoverage() {
+        SecondFragment fragment = new SecondFragment();
+        fragment.onDestroyView();
     }
 }
