45bc498996fd947e43f5acd13c36f48ee423630b
Jason Ernst <ernstjason1@gmail.com>
2023-05-10 22:38:44 -0700

    fix for mulitple devices

--- scripts/clean-packages.sh
+++ scripts/clean-packages.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-timeout 20 adb wait-for-device
-
 for DEVICE in $(adb devices | sed '1d' | sed '$d' | awk '{print $1}'); do
     timeout 20 adb -s $DEVICE wait-for-device
     for i in $(adb -s $DEVICE shell pm list packages -f | grep com.example.myapplication | sed -n 's/^.*\/base.apk=//p'); do
