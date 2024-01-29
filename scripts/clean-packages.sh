0773ee85d7df115da4e8c6ce949426fce8bbb9a2
Jason Ernst <ernstjason1@gmail.com>
2024-01-29 10:12:42 -0800

    Added a minimum device count check

--- scripts/clean-packages.sh
+++ scripts/clean-packages.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
+# this is a bit complicated because bash isn't great at handling pipes, all of this just gets the
+# number of devices that adb is showing into the NUM_DEVICES variable See:
+# https://unix.stackexchange.com/questions/338000/bash-assign-output-of-pipe-to-a-variable
+countDevices() {
+  timeout 20 adb devices | sed '1d' | sed '$d' | awk '{print $1}' | wc -l
+}
+{ read -d '' count_device; } < <(countDevices)
+
+NUM_DEVICES=${count_device}
+if [ $NUM_DEVICES -lt 1 ]; then
+    echo "This test request at least one device, we found $NUM_DEVICES"
+    exit 1
+fi
+
 for DEVICE in $(adb devices | sed '1d' | sed '$d' | awk '{print $1}'); do
     timeout 20 adb -s $DEVICE wait-for-device
     for i in $(adb -s $DEVICE shell pm list packages -f | grep com.example.myapplication | sed -n 's/^.*\/base.apk=//p'); do
